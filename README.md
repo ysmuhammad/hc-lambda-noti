2c52f2e83b65c24bb973fdd76dbc15b96f60a891
yusuftravlr <yusuf.travlr@gmail.com>
2021-08-27 10:56:41 +0700

    add environment variable

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Simple python to send notification to Slack when Route53 Health Check is Unhealthy
+# Route53 Health Check Notification
+
+Simple python script to send notification to Slack when Route53 Health Check is unhealthy.
+
+Don't forget to add `SLACK_WEBHOOK_URL` environment variable and set it accordingly.
+
--- notification.py
+++ notification.py
@@ -1,39 +1,41 @@
+import os
 import urllib3
 import json
 
 http = urllib3.PoolManager()
 
+
 def composeMsg(msg, alarm):
     if alarm == "ALARM":
         alarmName = json.loads(msg['Records'][0]['Sns']['Message'])
         message = str(
-            "*Alarm Name:* " + "`" + alarmName['AlarmName'] + "`" + 
-            "\n*Status:* " + "`" + alarmName['NewStateValue'] + "`" + 
-            "\n*Description:* " + "Health check is unhealthy!" + 
-            "\n*Timestamp:* " + msg['Records'][0]['Sns']['Timestamp'] + 
-            "\n*Details:*" + "```" + alarmName['NewStateReason'] + "```" 
+            "*Alarm Name:* " + "`" + alarmName['AlarmName'] + "`" +
+            "\n*Status:* " + "`" + alarmName['NewStateValue'] + "`" +
+            "\n*Description:* " + "Health check is unhealthy!" +
+            "\n*Timestamp:* " + msg['Records'][0]['Sns']['Timestamp'] +
+            "\n*Details:*" + "```" + alarmName['NewStateReason'] + "```"
             "\n*Alarm Link:* " + "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#alarmsV2:alarm/" + alarmName['AlarmName'])
         return message
-    if alarm == "OK":        
+    if alarm == "OK":
         alarmName = json.loads(msg['Records'][0]['Sns']['Message'])
         message = str(
-            "*Alarm Name:* " + "`" + alarmName['AlarmName'] + "`" + 
-            "\n*Status:* " + "`" + alarmName['NewStateValue'] + "`" + 
-            "\n*Description:* " + "Health check is healthy!" + 
-            "\n*Timestamp:* " + msg['Records'][0]['Sns']['Timestamp'] + 
-            "\n*Details:*" + "```" + alarmName['NewStateReason'] + "```" 
+            "*Alarm Name:* " + "`" + alarmName['AlarmName'] + "`" +
+            "\n*Status:* " + "`" + alarmName['NewStateValue'] + "`" +
+            "\n*Description:* " + "Health check is healthy!" +
+            "\n*Timestamp:* " + msg['Records'][0]['Sns']['Timestamp'] +
+            "\n*Details:*" + "```" + alarmName['NewStateReason'] + "```"
             "\n*Alarm Link:* " + "https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#alarmsV2:alarm/" + alarmName['AlarmName'])
         return message
 
+
 def lambda_handler(event, context):
-    url = "<PUT_YOUR_SLACK_WEBHOOK_URL>"
+    url = os.environ["SLACK_WEBHOOK_URL"]
 
     responseStatus = str(event['Records'][0]['Sns']['Subject'])
     if "ALARM" in responseStatus:
         title = str("*" + event['Records'][0]['Sns']['Subject'] + "*")
         slackMsg = composeMsg(event, "ALARM")
         msg = {
-            "channel": "<CHANNEL_NAME>",
             "text": title,
             "attachments": [{
                 "color": "#961e13",
@@ -41,14 +43,13 @@ def lambda_handler(event, context):
                     "value": slackMsg,
                 }]
             }]
-            
+
         }
-    
+
     if "OK" in responseStatus:
         title = str("*" + event['Records'][0]['Sns']['Subject'] + "*")
         slackMsg = composeMsg(event, "OK")
         msg = {
-            "channel": "<CHANNEL_NAME>",
             "text": title,
             "attachments": [{
                 "color": "#5cb589",
@@ -56,12 +57,12 @@ def lambda_handler(event, context):
                     "value": slackMsg,
                 }]
             }]
-            
+
         }
     encoded_msg = json.dumps(msg).encode('utf-8')
-    resp = http.request('POST',url, body=encoded_msg)
+    resp = http.request('POST', url, body=encoded_msg)
     print({
-        "message": event['Records'][0]['Sns']['Message'], 
-        "status_code": resp.status, 
+        "message": event['Records'][0]['Sns']['Message'],
+        "status_code": resp.status,
         "response": resp.data
     })
